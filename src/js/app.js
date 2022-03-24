f1d3e8ac52f9d1492b1eecb3e20bc76c757df6ac
Jenya <Ocupuc123@mail.ru>
2022-03-24 20:28:11 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 // import mmenuInit from './modules/mmenu';
-// import sliderInit from "./modules/swiper";
+// import sliderInit from './modules/swiper';
 // import './modules/dynamic-adapt';
 // import './modules/bootstrap';
 // import './modules/beerslider';
