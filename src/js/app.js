a53d5cd127114f2a0b5fec0bbd9660eb699b1a99
Jenya <Ocupuc123@mail.ru>
2022-09-28 18:13:27 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   $('.slick-slider').slick();
+
+  const dropdownButtons = document.querySelectorAll('.main-nav .chevron');
+
+  if (dropdownButtons.length > 0) {
+    dropdownButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const parentElement = button.parentElement;
+        parentElement.classList.toggle('is-active');
+      });
+    });
+  }
 });
