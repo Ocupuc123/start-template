c4524b60b4301d1f3dc08a5f28d08bf3e84d3afd
Jenya <Ocupuc123@mail.ru>
2022-11-22 23:41:42 +0300

    upd

--- gulp/browser-sync.mjs
+++ gulp/browser-sync.mjs
@@ -3,7 +3,6 @@ import server from 'browser-sync';
 import { pugMixins } from './pug-mixins.mjs';
 import { pugToHtml } from './pug-to-html.mjs';
 import { styles } from './styles.mjs';
-import { bootstrapStyles } from './bootstrap-styles.mjs';
 import { vendorsCss, vendorsJs } from './vendors.mjs';
 import { fonts } from './fonts.mjs';
 import { resources } from './resources.mjs';
@@ -22,7 +21,6 @@ const browserSync = (callback) => {
   gulp.watch('src/vendors/**/*.*', gulp.series(vendorsCss, vendorsJs));
   gulp.watch(['src/**/*.pug', '!src/blocks/mixins.pug'], gulp.series(pugMixins, pugToHtml));
   gulp.watch(['src/**/*.scss', '!src/scss/_variables.scss', '!src/scss/bootstrap/*.scss'], gulp.series(styles));
-  gulp.watch('src/scss/_variables.scss', gulp.series(bootstrapStyles));
   gulp.watch('src/fonts/**/*.*', gulp.series(fonts));
   gulp.watch('src/resources/**/*.*', gulp.series(resources));
   gulp.watch('src/images/icons/svg/*.svg', gulp.series(svgSprite));
