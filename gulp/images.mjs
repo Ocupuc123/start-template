ae7638fa821f65ebe3f338a2d730181db074fc9c
Jenya <Ocupuc123@mail.ru>
2022-11-22 10:10:44 +0300

    upd

--- gulp/images.mjs
+++ gulp/images.mjs
@@ -1,20 +1,21 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
-import imagemin from 'gulp-imagemin';
+import imagemin, {mozjpeg, optipng} from 'gulp-imagemin';
 import webp from 'gulp-webp';
 import browsersync from 'browser-sync';
 
 
 const images = () => gulp.src(['src/images/**/*.{gif,png,jpg,jpeg,webp}', '!src/images/icons/**/*.*', '!src/images/favicons/**/*.*'])
   .pipe(plumber())
-  .pipe(webp())
+  .pipe(webp({
+    quality: 95
+  }))
   .pipe(gulp.dest('build/images'))
   .pipe(gulp.src(['src/images/**/*.{gif,png,jpg,jpeg,webp}', '!src/images/icons/**/*.*', '!src/images/favicons/**/*.*']))
-  .pipe(imagemin({
-    progressive: true,
-    interlaced: true,
-    optimizationLevel: 3
-  }))
+  .pipe(imagemin([
+    mozjpeg({ quality: 98, progressive: true }),
+    optipng({optimizationLevel: 1}),
+  ]))
   .pipe(gulp.dest('build/images'))
   .pipe(gulp.src('src/images/*.svg'))
   .pipe(gulp.dest('build/images'))
