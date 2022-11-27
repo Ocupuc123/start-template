cf4ad3f76c922cefc7a48e53c976eb4ea4e79fbe
Jenya <Ocupuc123@mail.ru>
2022-11-27 19:12:53 +0300

    upd

--- gulp/pug-to-html.mjs
+++ gulp/pug-to-html.mjs
@@ -28,9 +28,10 @@ const getClassesToBlocksList = () => through.obj(function (file, enc, cb) {
       .toString()
       .split('\n')
       .map((html) => {
-        const blocksFolder = fs.readdirSync(`${process.cwd()}${path.sep}src${path.sep}blocks${path.sep}`, {
-          withFileTypes: true
-        }).filter((d) => d.isDirectory()).map((d) => d.name);
+        // const blocksFolder = fs.readdirSync(`${process.cwd()}${path.sep}src${path.sep}blocks${path.sep}`, {
+        //   withFileTypes: true
+        // }).filter((d) => d.isDirectory()).map((d) => d.name);
+        const isEmptyFile = fs.readdirSync(`${process.cwd()}${path.sep}src${path.sep}blocks${path.sep}`);
         const fileContent = html;
         const classesInFile = getClassesFromHtml(fileContent);
 
