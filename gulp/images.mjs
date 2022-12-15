6a3ced01faf4455c10f25a28c28574104f89bc50
Jenya <Ocupuc123@mail.ru>
2022-12-15 13:16:17 +0300

    upd

--- gulp/images.mjs
+++ gulp/images.mjs
@@ -7,8 +7,8 @@ import browsersync from 'browser-sync';
 const images = () => gulp.src(['src/images/**/*.{gif,png,jpg,jpeg,webp}', '!src/images/icons/**/*.*', '!src/images/favicons/**/*.*'])
   .pipe(plumber())
   .pipe(imagemin([
-    mozjpeg({ quality: 90, progressive: true }),
-    optipng({optimizationLevel: 1}),
+    mozjpeg({ quality: 80, progressive: true }),
+    optipng({optimizationLevel: 2}),
   ]))
   .pipe(gulp.dest('build/images'))
   .pipe(gulp.src('src/images/*.svg'))
