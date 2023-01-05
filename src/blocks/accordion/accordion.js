ed529e81de8488cf054f4dd42e2d3a78c02989c3
Jenya <Ocupuc123@mail.ru>
2023-01-05 15:23:38 +0300

    Добавил блок аккордион

---
+++ src/blocks/accordion/accordion.js
@@ -0,0 +1,16 @@
+import Accordion from 'accordion-js';
+
+const accordions = Array.from(document.querySelectorAll('.accordion'));
+
+if (accordions.length) {
+  const accordion = new Accordion(accordions, {
+    elementClass: 'accordion__item',
+    triggerClass: 'accordion__trigger',
+    panelClass: 'accordion__panel',
+    activeClass: 'accordion__item--active',
+    duration: 400,
+  });
+  accordion.forEach((item) => {
+    item.open(0);
+  });
+}
