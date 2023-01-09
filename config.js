ec5ce8905018129cb44cc14bf9a9f43b1b9d5146
Jenya <Ocupuc123@mail.ru>
2023-01-09 17:26:21 +0300

    upd

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const config = {
   ],
   'addAssets': {
     'src/fonts/*.*': 'fonts/',
-    'src/images/favicons/*.{png,ico,svg,xml,webmanifest}': 'images/favicons/',
+    'src/images/favicons/*.*': 'images/favicons/',
     // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'images/',
   },
 };
