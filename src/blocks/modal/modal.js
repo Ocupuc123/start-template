7d13e1b39d3ca8510e94898ac19df3030cba988c
Jenya <Ocupuc123@mail.ru>
2023-01-31 17:13:37 +0300

    upd

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -13,35 +13,41 @@ document.addEventListener('click', (evt) => {
   if (target && target.dataset.modal === 'close' || evt.target.matches('[aria-modal]')) {
     closeAllModals();
   }
+});
 
-  function showModal(targetModalNode) {
-    if ((document.body.clientHeight - document.documentElement.clientHeight) > 0) {
-      document.body.style.paddingRight = `${bodyPaddingRightOriginal + getScrollSize() }px`;
-    }
-    document.body.classList.add('modal-open');
-
-    targetModalNode.classList.add('modal--show');
-    targetModalNode.style.display = 'block';
-    targetModalNode.ariaModal = true;
-    targetModalNode.ariaHidden = null;
-    targetModalNode.setAttribute('role', 'dialog');
-
-    backdrop.className = 'modal-backdrop';
-    document.body.append(backdrop);
-  }
-
-  function closeAllModals() {
-    document.body.classList.remove('modal-open');
-    document.body.style.paddingRight = '';
-
-    document.querySelectorAll('.modal').forEach((modal) => {
-      modal.classList.remove('modal--show');
-      modal.style.display = 'none';
-      modal.ariaModal = null;
-      modal.ariaHidden = true;
-      modal.removeAttribute('role');
-    });
-
-    backdrop.remove();
+document.addEventListener('keydown', (evt) => {
+  if (evt.code === 'Escape') {
+    closeAllModals();
   }
 });
+
+function showModal(targetModalNode) {
+  if ((document.body.clientHeight - document.documentElement.clientHeight) > 0) {
+    document.body.style.paddingRight = `${bodyPaddingRightOriginal + getScrollSize() }px`;
+  }
+  document.body.classList.add('modal-open');
+
+  targetModalNode.classList.add('modal--show');
+  targetModalNode.style.display = 'block';
+  targetModalNode.ariaModal = true;
+  targetModalNode.ariaHidden = null;
+  targetModalNode.setAttribute('role', 'dialog');
+
+  backdrop.className = 'modal-backdrop';
+  document.body.append(backdrop);
+}
+
+function closeAllModals() {
+  document.body.classList.remove('modal-open');
+  document.body.style.paddingRight = '';
+
+  document.querySelectorAll('.modal').forEach((modal) => {
+    modal.classList.remove('modal--show');
+    modal.style.display = 'none';
+    modal.ariaModal = null;
+    modal.ariaHidden = true;
+    modal.removeAttribute('role');
+  });
+
+  backdrop.remove();
+}
