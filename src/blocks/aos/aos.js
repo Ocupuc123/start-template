b3c9ba14ada5aa4abf2d1c7961a60934bfed78d3
Jenya <Ocupuc123@mail.ru>
2023-04-06 18:47:14 +0300

    upd

---
+++ src/blocks/aos/aos.js
@@ -0,0 +1,7 @@
+// https://github.com/michalsnik/aos/tree/v2
+
+import AOS from 'aos';
+
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init();
+});
--- src/blocks/swiper/swiper.js
+++ src/blocks/swiper/swiper.js
@@ -3,36 +3,39 @@
 /* eslint-disable no-unused-vars */
 import Swiper from 'swiper/bundle';
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  slidesPerView: 1,
-  loop: true,
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
-    480: {
-      slidesPerView: 3,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    640: {
-      slidesPerView: 4,
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 3,
+      },
+      640: {
+        slidesPerView: 4,
+      }
     }
-  }
+  });
+
 });
