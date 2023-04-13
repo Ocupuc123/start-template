fe3c7b9fec3fd9d58b7482c3854ef81ca7f6a446
Jenya <Ocupuc123@mail.ru>
2023-04-13 12:01:02 +0300

    upd

--- createBlock.js
+++ createBlock.js
@@ -1,10 +1,9 @@
-/* eslint-disable no-console */
 /* global process */
 // Генератор файлов блока
 // Использование: node createBlock.js [имя блока] [доп. расширения через пробел]
 
 import fs from 'node:fs';
-import mkdirp from 'mkdirp';
+import {mkdirp} from 'mkdirp';
 const blockName = process.argv[2];
 const defaultExtensions = ['scss', 'img']; // расширения по умолчанию
 const extensions = uniqueArray(defaultExtensions.concat(process.argv.slice(3)));
@@ -14,7 +13,8 @@ if (blockName) {
   const dirPath = `src/blocks/${blockName}/`; // полный путь к создаваемой папке блока
 
   const made = mkdirp.sync(dirPath);
-  console.log(`[NTH] Создание папки: ${made}`);
+  // eslint-disable-next-line no-console
+  console.log(`Создание папки: ${made}`);
 
   // Обходим массив расширений и создаем файлы, если они еще не созданы
   extensions.forEach((extension) => {
@@ -24,58 +24,58 @@ if (blockName) {
 
     if (extension === 'scss') {
       fileContent = `// В этом файле должны быть стили для БЭМ-блока ${blockName}, его элементов,\n// модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n// Очередность: http://nicothin.github.io/idiomatic-pre-CSS/#priority\n\n.${blockName} {\n  $${blockName}: &; // #{$${blockName}}__element\n}\n`;
-    }
-
-    else if (extension === 'js') {
+    } else if (extension === 'js') {
       fileContent = '';
-    }
-
-    else if (extension === 'md') {
+    } else if (extension === 'md') {
       fileContent = '';
-    }
-
-    else if (extension === 'pug') {
+    } else if (extension === 'pug') {
       fileContent = `//- Все примеси в этом файле должны начинаться c имени блока (${blockName})\n\nmixin ${blockName}(text, mods)\n\n  //- Принимает:\n  //-   text    {string} - текст\n  //-   mods    {string} - список модификаторов\n  //- Вызов:\n        +${blockName}('Текст', 'some-mod')\n\n  -\n    // список модификаторов\n    var allMods = '';\n    if(typeof(mods) !== 'undefined' && mods) {\n      var modsList = mods.split(',');\n      for (var i = 0; i < modsList.length; i++) {\n        allMods = allMods + ' ${blockName}--' + modsList[i].trim();\n      }\n    }\n\n  .${blockName}(class=allMods)&attributes(attributes)\n    .${blockName}__inner\n      block\n`;
-    }
-
-    else if (extension === 'img') {
+    } else if (extension === 'img') {
       const imgFolder = `${dirPath}img/`;
       if (fileExist(imgFolder) === false) {
         const madeImgFolder = mkdirp.sync(imgFolder);
-        console.log(`[NTH] Создание папки: ${madeImgFolder}`);
+        // eslint-disable-next-line no-console
+        console.log(`Создание папки: ${madeImgFolder}`);
       } else {
-        console.log(`[NTH] Папка ${imgFolder} НЕ создана (уже существует) `);
+        // eslint-disable-next-line no-console
+        console.log(`Папка ${imgFolder} НЕ создана (уже существует) `);
       }
     }
 
     if (fileExist(filePath) === false && extension !== 'img' && extension !== 'md') {
       fs.writeFile(filePath, fileContent, (err) => {
         if (err) {
-          return console.log(`[NTH] Файл НЕ создан: ${err}`);
+          // eslint-disable-next-line no-console
+          return console.log(`Файл НЕ создан: ${err}`);
         }
-        console.log(`[NTH] Файл создан: ${filePath}`);
+        // eslint-disable-next-line no-console
+        console.log(`Файл создан: ${filePath}`);
         if (fileCreateMsg) {
+          // eslint-disable-next-line no-console
           console.warn(fileCreateMsg);
         }
       });
-    }
-    else if (extension !== 'img' && extension !== 'md') {
-      console.log(`[NTH] Файл НЕ создан: ${filePath} (уже существует)`);
-    }
-    else if (extension === 'md') {
+    } else if (extension !== 'img' && extension !== 'md') {
+      // eslint-disable-next-line no-console
+      console.log(`Файл НЕ создан: ${filePath} (уже существует)`);
+    } else if (extension === 'md') {
       fs.writeFile(`${dirPath}readme.md`, fileContent, (err) => {
         if (err) {
-          return console.log(`[NTH] Файл НЕ создан: ${err}`);
+          // eslint-disable-next-line no-console
+          return console.log(`Файл НЕ создан: ${err}`);
         }
-        console.log(`[NTH] Файл создан: ${dirPath}readme.md`);
+        // eslint-disable-next-line no-console
+        console.log(`Файл создан: ${dirPath}readme.md`);
         if (fileCreateMsg) {
+          // eslint-disable-next-line no-console
           console.warn(fileCreateMsg);
         }
       });
     }
   });
 } else {
-  console.log('[NTH] Отмена операции: не указан блок');
+  // eslint-disable-next-line no-console
+  console.log('Отмена операции: не указан блок');
 }
 
 function uniqueArray(arr) {
