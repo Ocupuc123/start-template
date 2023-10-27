481364d7293d7307e8a6467c0b5bb64112eb6fed
Jenya <Ocupuc123@mail.ru>
2023-10-27 21:51:03 +0300

    upd

---
+++ src/blocks/smartmenu/smartmenu.js
@@ -0,0 +1,10 @@
+import smartmenus from 'smartmenus';
+
+jQuery(() => {
+  jQuery('.smartmenu').smartmenus({
+    mainMenuSubOffsetX: -1,
+    mainMenuSubOffsetY: 4,
+    subMenusSubOffsetX: 6,
+    subMenusSubOffsetY: -6
+  });
+});
