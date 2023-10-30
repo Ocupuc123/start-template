57da5cd839cd8abe3bfe7c037f29a88c1684d03f
Jenya <Ocupuc123@mail.ru>
2023-10-30 15:16:30 +0300

    upd

---
+++ src/blocks/mobile-nav/mobile-nav.js
@@ -0,0 +1,49 @@
+import { getScrollSize } from '../../js/utils/get-scrol-size.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bodyPaddingRightOriginal = parseInt(window.getComputedStyle(document.body, null).getPropertyValue('padding-right'), 10);
+  const backdrop = document.createElement('div');
+
+  document.addEventListener('click', (event) => {
+    const target = event.target.closest('a[data-mobile-nav], button[data-mobile-nav]');
+
+    if (target && target.dataset.mobileNav === 'open') {
+      showMobileNav(document.getElementById((target.hash || target.dataset.mobileNavTarget).slice(1)));
+    }
+
+    if (target && target.dataset.mobileNav === 'close' || event.target.matches('[aria-modal]')) {
+      closeMobileNavs();
+    }
+
+    function showMobileNav(targetModalNode) {
+      if ((document.body.clientHeight - document.documentElement.clientHeight) > 0) {
+        document.body.style.paddingRight = `${bodyPaddingRightOriginal + getScrollSize() }px`;
+      }
+      document.body.classList.add('mobile-nav-open');
+
+      targetModalNode.classList.add('mobile-nav--show');
+      targetModalNode.style.display = 'block';
+      targetModalNode.ariaModal = true;
+      targetModalNode.ariaHidden = null;
+      targetModalNode.setAttribute('role', 'dialog');
+
+      backdrop.className = 'mobile-nav-backdrop';
+      document.body.append(backdrop);
+    }
+
+    function closeMobileNavs() {
+      document.body.classList.remove('mobile-nav-open');
+      document.body.style.paddingRight = '';
+
+      const mobileNav = document.querySelector('.mobile-nav');
+
+      mobileNav.classList.remove('mobile-nav--show');
+      mobileNav.style.display = 'none';
+      mobileNav.ariaModal = null;
+      mobileNav.ariaHidden = true;
+      mobileNav.removeAttribute('role');
+
+      backdrop.remove();
+    }
+  });
+});
