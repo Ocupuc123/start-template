a213d9bb2466126ffbd3dd834a8439d3e634a2d2
Jenya <Ocupuc123@mail.ru>
2023-12-13 17:11:44 +0300

    upd

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: ['@prettier/plugin-pug'],
-
-  pugPrintWidth: 120,
-  pugSingleQuote: true,
-  PugBracketLine: true,
-  pugBracketSpacing: true,
-  pugBracketSameLine: true
-};
