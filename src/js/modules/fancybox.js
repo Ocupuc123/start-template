81531b1a015b1de01c4a0f958d268fa77186c3d3
Jenya <Ocupuc123@mail.ru>
2025-04-24 17:23:55 +0300

    upd

--- src/js/modules/fancybox.js
+++ src/js/modules/fancybox.js
@@ -1,3 +1,21 @@
 import { Fancybox } from '@fancyapps/ui';
 
 Fancybox.bind('[data-fancybox]');
+
+const fancyboxLinks = document.querySelectorAll('a[data-fancybox][href*="vkvideo.ru"]');
+
+if (fancyboxLinks.length > 0) {
+  fancyboxLinks.forEach((link) => {
+    const originalHref = link.getAttribute('href');
+
+    if (originalHref.includes('vkvideo.ru/video-') && originalHref.match(/video-\d+_\d+/)) {
+      const ids = originalHref.match(/video-(\d+)_(\d+)/);
+      const oid = ids[1];
+      const vid = ids[2];
+
+      const newHref = `https://vkvideo.ru/video_ext.php?oid=-${oid}&id=${vid}`;
+
+      link.setAttribute('href', newHref);
+    }
+  });
+}
